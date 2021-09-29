f964780535d4f633db466c58b5b0e918fda69adf
df@ <@>
2021-09-29 22:50:49 +0100

    ..

--- game_of_life/src/main.rs
+++ game_of_life/src/main.rs
@@ -1,6 +1,11 @@
 extern crate rand;
+extern crate termion;
 extern crate image;
-use std::{thread, time};
+use std::{env, thread, time};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use termion::color;
+use termion::clear;
 
 fn census(_world: [[u8; 75]; 75]) -> u16 {
     let mut count = 0;
@@ -68,17 +73,49 @@ fn main() {
     let mut world = [[0_u8; 75]; 75];
     let mut generations = 0;
 
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        for i in 0..74 {
+            for j in 0..74 {
+                if rand::random() {
+                    world[i][j] = 1;
+                } else {
+                    world[i][j] = 0;
+                }
+            }
+        }
+        
+    } else {
+
+        let filename = env::args().nth(1).unwrap();
+        let world = populate_from_file(filename);
+    }
+    write_image(world);
+}
+
+fn populate_from_file(filename: String) -> [[u8; 75]; 75] {
+    let mut newworld = [[0_u8; 75]; 75];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut pairs: Vec<(usize, usize)> = Vec::new();
+    for (index, line) in reader.lines().enumerate() {
+        let l = line.unwrap();
+        let mut words = l.split_whitespace();
+        let left = words.next().unwrap();
+        let rigth = words.next().unwrap();
+        pairs.push((left.parse::<usize>().unwrap(), rigth.parse::<usize>().unwrap()))
+    }
     for i in 0..74 {
         for j in 0..74 {
-            if rand::random() {
-                world[i][j] = 1;
-            } else {
-                world[i][j] = 0;
-            }
+            newworld[i][j] = 0;
         }
     }
 
-    write_image(world);
+    for (x,y) in pairs {
+        newworld[x][y] = 1;
+    }
+
+    newworld
 }
 
 fn write_image(pixels: [[u8; 75]; 75]) {
