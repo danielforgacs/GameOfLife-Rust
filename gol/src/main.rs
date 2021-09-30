876f900a17aece3ac1066cc5b46aff51fcb19234
df@ <@>
2021-09-30 22:28:43 +0100

    generations works.

--- gol/src/main.rs
+++ gol/src/main.rs
@@ -1,13 +1,23 @@
-const WIDTH: u8 = 8;
-const HEIGHT: u8 = 2;
+const WIDTH: u8 = 32;
+const HEIGHT: u8 = 16;
+const LIFE_MINIMUM: f64 = 0.8;
+
+const LIVE: char = 'O';
+const DEAD: char = ' ';
 
 fn main() {
     let mut cellcount = 0;
     for y in 0..HEIGHT {
-        print!("\n");
         for x in 0..WIDTH {
             cellcount += 1;
-            print!("{}({},{}) ", cellcount, x, y);
+            let life = rand::random::<f64>() > LIFE_MINIMUM;
+            // print!("{}({},{}){} ", cellcount, x, y, life);
+            match life {
+                true => { print!("{}", LIVE) }
+                false => { print!("{}", DEAD) }
+            }
+            
         }
+        print!("\n");
     }
 }
