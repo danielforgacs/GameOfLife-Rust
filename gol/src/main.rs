c91bf0a986bb4be8339115de08c19f3bc6d373e1
df@ <@>
2021-09-30 22:41:41 +0100

    ok

--- gol/src/main.rs
+++ gol/src/main.rs
@@ -5,7 +5,7 @@ struct Parms {
     width: u8,
     height: u8,
     life_minimum: f64,
-    max_gen: u8,
+    generations: u8,
 }
 
 fn main() {
@@ -13,22 +13,27 @@ fn main() {
         width: 16,
         height: 8,
         life_minimum: 0.8,
-        max_gen: 3,
+        generations: 3,
     };
+
     let mut cellcount = 0;
-    for y in 0..parms.height {
-        for x in 0..parms.width {
-            cellcount += 1;
-            let life = rand::random::<f64>() > parms.life_minimum;
-            match life {
-                true => {
-                    print!("{}", LIVE)
-                }
-                false => {
-                    print!("{}", DEAD)
+    let mut generation = 0;
+
+    for gen in 0..parms.generations {
+        for y in 0..parms.height {
+            for x in 0..parms.width {
+                cellcount += 1;
+                let life = rand::random::<f64>() > parms.life_minimum;
+                match life {
+                    true => {
+                        print!("{}", LIVE)
+                    }
+                    false => {
+                        print!("{}", DEAD)
+                    }
                 }
             }
+            print!("\n");
         }
-        print!("\n");
     }
 }
