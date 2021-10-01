336871b885243540a8de1c2a87ed1692bae97d3a
df@ <@>
2021-10-01 11:25:51 +0100

    refactored cell life to be enum

--- gol/src/main.rs
+++ gol/src/main.rs
@@ -10,6 +10,11 @@ const LIFE_LIMIT_ARGS_INDEX: usize = 1;
 const GENERATIONS_ARGS_INDEX: usize = 2;
 const WIDTH_ARGS_INDEX: usize = 3;
 
+enum CellLife {
+    Alive,
+    Dead,
+}
+
 
 struct Parms {
     width: u8,
@@ -62,13 +67,17 @@ fn main() {
     }
 }
 
-fn generate_map(parms: &Parms) -> Vec<Vec<bool>> {
-    let mut map: Vec<Vec<bool>> = Vec::new();
+fn generate_map(parms: &Parms) -> Vec<Vec<CellLife>> {
+    let mut map: Vec<Vec<CellLife>> = Vec::new();
 
     for _y in 0..parms.height {
-        let mut row: Vec<bool> = Vec::new();
+        let mut row: Vec<CellLife> = Vec::new();
         for _x in 0..parms.width {
             let life = rand::random::<f64>() > parms.life_minimum;
+            let life = match life {
+                true => CellLife::Alive,
+                _ => CellLife::Dead,
+            };
             row.push(life);
         }
         map.push(row);
@@ -76,39 +85,43 @@ fn generate_map(parms: &Parms) -> Vec<Vec<bool>> {
     map
 }
 
-fn display_map(map: &Vec<Vec<bool>>, gen: &u16) {
+fn display_map(map: &Vec<Vec<CellLife>>, gen: &u16) {
     print!("generation: {}\n", gen);
     for y in map {
         for x in y {
             print!("{}", {
-                if *x == true {
-                    LIVE
-                } else {
-                    DEAD
+                match *x {
+                    CellLife::Alive => LIVE,
+                    _ => DEAD,
                 }
+                // if *x == true {
+                //     LIVE
+                // } else {
+                //     DEAD
+                // }
             });
         }
         print!("\n");
     }
 }
 
-fn calc_next_gen_map(map: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    let mut newmap: Vec<Vec<bool>> = Vec::new();
+fn calc_next_gen_map(map: &Vec<Vec<CellLife>>) -> Vec<Vec<CellLife>> {
+    let mut newmap: Vec<Vec<CellLife>> = Vec::new();
 
     for (y, row) in map.iter().enumerate() {
-        let mut newrow: Vec<bool> = Vec::new();
+        let mut newrow: Vec<CellLife> = Vec::new();
         for (x, _cell) in row.iter().enumerate() {
             let neighbour_count = count_cell_neighbours(map, &x, &y);
-            if map[y][x] == true {
+            if map[y][x] == CellLife::Alive {
                 match neighbour_count {
-                    0 | 1 => newrow.push(false),
-                    2 | 3 => newrow.push(true),
-                    _ => newrow.push(false),
+                    0 | 1 => newrow.push(CellLife::Dead),
+                    2 | 3 => newrow.push(CellLife::Alive),
+                    _ => newrow.push(CellLife::Dead),
                 }
             } else {
                 match neighbour_count {
-                    3 | 4 => newrow.push(true),
-                    _ => newrow.push(false),
+                    3 | 4 => newrow.push(CellLife::Alive),
+                    _ => newrow.push(CellLife::Dead),
                 }
             }
         }
@@ -118,7 +131,7 @@ fn calc_next_gen_map(map: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     newmap
 }
 
-fn count_cell_neighbours(map: &Vec<Vec<bool>>, x: &usize, y: &usize) -> u16 {
+fn count_cell_neighbours(map: &Vec<Vec<CellLife>>, x: &usize, y: &usize) -> u16 {
     let mut count = 0;
     let coords: Vec<i16> = vec![-1, 0, 1];
 
@@ -139,7 +152,7 @@ fn count_cell_neighbours(map: &Vec<Vec<bool>>, x: &usize, y: &usize) -> u16 {
                 continue;
             }
 
-            if map[ny][nx] == true {
+            if map[ny][nx] == CellLife::Alive {
                 count += 1;
             }
         }
