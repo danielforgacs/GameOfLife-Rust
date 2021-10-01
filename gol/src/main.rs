2c0b79979aa045700514f9309454212917730287
df@ <@>
2021-10-01 21:10:50 +0100

    OK

--- gol/src/main.rs
+++ gol/src/main.rs
@@ -138,22 +138,26 @@ fn main() {
         world.generation, world.alive_count()
     );
     world.display();
-    world.next_generation();
-    print!(
-        "generation: {}\n\
-        alive count: {}\n",
-        world.generation, world.alive_count()
-    );
-    world.display();
+
+    for _k in 0..parms.generations {
+
+        world.next_generation();
+        print!(
+            "generation: {}\n\
+            alive count: {}\n",
+            world.generation, world.alive_count()
+        );
+        world.display();
+    }
     return;
 
     let mut map = generate_map(&parms);
     display_map(&map, &0_u16);
 
-    for gen in 1..parms.generations {
-        map = calc_next_gen_map(&map);
-        display_map(&map, &gen);
-    }
+    // for gen in 1..parms.generations {
+    //     map = calc_next_gen_map(&map);
+    //     display_map(&map, &gen);
+    // }
 
     print!(
         "dimexnsions: {dimx}x{dimy}\n\
